95d14a1b4859f763c036dc2e4e5fc4b8bea2eed5
cerise77 <vanass567@gmail.com>
2017-04-10 12:38:14 +0300

    eru

--- index.js
+++ index.js
@@ -22,3 +22,11 @@ app.listen(app.get('port'), function() {
 });*/
 
 Server.start(port, hostname, info);
+
+
+/*const Server = require('./server/server4');
+//
+const hostname = '127.0.0.1';
+const port = 3000;
+let info = "\nnode-static-http-server by Janus Nic\nExamples of HTTP static file serving in Node.js\nSee: https://github.com/janusnic/web-dev-node\n";
+Server.start(port, hostname, info);*/
