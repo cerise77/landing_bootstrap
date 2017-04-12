1c253548eabd576e7cc3bd3dab115867c999f8d4
cerise77 <vanass567@gmail.com>
2017-04-12 14:35:18 +0300

    rtyu

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+let router = express.Router();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,12 +14,13 @@ app.set('view engine', 'ejs');
   response.render('pages/index');
 });*/
 
-/*app.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
-});*/
-app.get('/', (request, response) => {
-  response.send('Hello from Express!');
 });
+
+/*app.get('/', (request, response) => {
+  response.send('Hello from Express!');
+});*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
