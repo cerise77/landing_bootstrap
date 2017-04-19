cd9cda8c2f331e9f4945079fb22626e150d6bf93
cerise77 <vanass567@gmail.com>
2017-04-19 16:15:56 +0300

    djkfkjl

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 3000; 				// set the port
-var database = require('./config/database'); 			// load the database config
-var morgan   = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var express = require('express');
+var app = express();
+let rooot = require('./models');
+//let router = express.Router();
 
-// configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+app.set('port', (process.env.PORT || 5000));
 
-app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 
-// routes ======================================================================
-require('./app/routes.js')(app);
+/*app.get('/', function(request, response) {
+  response.render('pages/index');
+});*/
 
-// listen (start app with node server.js) ======================================
-// app.listen(port);
-// console.log("App listening on port " + port);
-
-// app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
+app.use('/', rooot);
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
-//Contact GitHub API Training Shop Blog About
-//© 2017 GitHub, Inc. Terms Privacy Security Status Help
+
+
+
+
+
+
+
+
+
+
+//app.use(require('./controllers')); // Монтируем контроллеры!
+
+/*app.get('/', function(request, response) {
+  response.render('pages/index');
+});*/
+
+/*app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/', function(request, response) {
+  response.render('pages/contacts');
+});*/
+
+/*app.get('/', (req, res, next) => {
+  res.send('Hello from Express!');
+});*/
+
+/*app.get('/', (req, res, next) => {
+  res.send('Hello from Express!');
+});*/
