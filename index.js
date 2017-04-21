3b38a57e23b6cdc8bcd167523ec5dbeb2846e74f
cerise77 <vanass567@gmail.com>
2017-04-21 14:59:51 +0300

    vngkg

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-let rooot = require('./models');
-//let router = express.Router();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,44 +9,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-
-/*app.get('/', function(request, response) {
+app.get('/', function(request, response) {
   response.render('pages/index');
-});*/
-
-app.use('/', rooot);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
-
-
-
-
-
-
-
-
-//app.use(require('./controllers')); // Монтируем контроллеры!
-
-/*app.get('/', function(request, response) {
-  response.render('pages/index');
-});*/
-
-/*app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/', function(request, response) {
-  response.render('pages/contacts');
-});*/
-
-/*app.get('/', (req, res, next) => {
-  res.send('Hello from Express!');
-});*/
-
-/*app.get('/', (req, res, next) => {
-  res.send('Hello from Express!');
-});*/
